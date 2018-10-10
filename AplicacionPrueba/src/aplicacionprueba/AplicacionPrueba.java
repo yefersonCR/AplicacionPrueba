9223c3dd52305a5daa8e7b9b8f340c41eda703ea
CESDE <CESDE@PC-03>
2018-10-10 08:44:33 -0500

    modificando mi proyecto

--- AplicacionPrueba/src/aplicacionprueba/AplicacionPrueba.java
+++ AplicacionPrueba/src/aplicacionprueba/AplicacionPrueba.java
@@ -16,6 +16,8 @@ public class AplicacionPrueba {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        // hola enrique
+        System.out.println("hola mundo");
     }
     
 }
